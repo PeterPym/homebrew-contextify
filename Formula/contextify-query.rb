3c91b57f46201f80ba7d60a43920919bb9577ecb
Rob Banagale <rob@banagale.com>
2025-12-23 00:05:39 -0800

    Update contextify-query to 1.0.6

--- Formula/contextify-query.rb
+++ Formula/contextify-query.rb
@@ -1,18 +1,18 @@
 class ContextifyQuery < Formula
   desc "CLI for querying Contextify database - enables Claude Code/Codex skills"
   homepage "https://contextify.sh"
-  version "1.0.5"
+  version "1.0.6"
   license "Proprietary"
 
   # Download pre-built binary from GitHub releases
   # Note: Currently arm64 only. x86_64 build will be added when needed.
   if Hardware::CPU.arm?
     url "https://github.com/PeterPym/contextify/releases/download/v#{version}/contextify-query-arm64.tar.gz"
-    sha256 "f53c98c6d9fd0e2dc5642291acc55cb3b0ebd977a31ce5c7083f9f3c6608813b"
+    sha256 "631a249f36c1ef3500848233b664b59ad513a7219383efed66ddc01e7ee28089"
   else
     # x86_64 binary not yet available - using arm64 for now (runs via Rosetta)
     url "https://github.com/PeterPym/contextify/releases/download/v#{version}/contextify-query-arm64.tar.gz"
-    sha256 "f53c98c6d9fd0e2dc5642291acc55cb3b0ebd977a31ce5c7083f9f3c6608813b"
+    sha256 "631a249f36c1ef3500848233b664b59ad513a7219383efed66ddc01e7ee28089"
   end
 
   depends_on :macos
